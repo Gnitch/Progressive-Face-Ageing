69a8d4bea7d481ff909463cdfdd2f694bb0efa8e
Gnitch <sujoydcunha@gmail.com>
2021-12-02 19:26:24 +0530

    initialize django project

---
+++ Finding_Missing_People/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Finding_Missing_People project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Finding_Missing_People.settings')
+
+application = get_asgi_application()
