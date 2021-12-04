f99515195ef01e3ec510ee6277d092a7141ef9e5
Gnitch <sujoydcunha@gmail.com>
2021-12-04 15:24:07 +0530

    base template completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from accounts.views import login, register
+
 app_name='accounts'
 urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
+    
+def register(request):
+    return render(request, 'accounts/register.html')    
